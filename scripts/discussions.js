2b00c935dad1faa0b5fe11efd313d54af160f999
Jovanni Feliz <kfeliz@c.ringling.edu>
2023-02-11 13:14:00 -0600

    imported image of praying Hands

--- scripts/discussions.js
+++ scripts/discussions.js
@@ -4,12 +4,6 @@ ticketLocation: "nss-evening-web-development/discussions-base",
 ticketName: "How To Prevent App From Automatically Crashing?",
 discussionPageName: "GhostZen_Help"
 },
-{
-ticketLocation: "nss-evening-web-development/discussions-base",
-ticketName: "How To Prevent App From Automatically Crashing?",
-discussionPageName: "GhostZen_Help"
-}
-
 ];
 
 const discussionToDOM = ( selectId, htmlRender) => {
@@ -26,16 +20,16 @@ const createDiscussion = (array) => {
     domString += 
     
    `<div class="overallDiscussionCard">
-      <div style="display: flex; border: solid gray 1px;">  
+      <div style="display: flex; border: solid rgb(48, 54, 61) 1px; padding: 15px">  
           <div class="mainDiscussLinks">
 
             <div class="discussTitle">
-            <a href=""><span style="color:gray; padding-right:10px;">${item.ticketLocation}</span></a>
-            <a href=""><span style="color:white;">${item.ticketName}</span></a>
+            <a href="" style="text-decoration: none; color:gray; padding-right:10px;">${item.ticketLocation}</span></a>
+            <a href="" style="text-decoration: none; color:white;">${item.ticketName}</a>
             </div>
-            <div style="background-color: red;">
-            <img src="">
-            <a href="">${item.discussionPageName}</a>
+            <div>
+            <img src="Images/prayingHands.png">
+            <a href="" style="text-decoration: none; color: rgb(111, 116, 121)">${item.discussionPageName}</a>
             </div>
             
           </div>
@@ -43,15 +37,15 @@ const createDiscussion = (array) => {
           <div class="iconPosition" >
 
             <div style="padding: 10px; box-sizing: content-box;">
-            <img src="Images/Monica.jpg" style="border-radius: 2000px; height: 40px; width: 40px;">
+            <img src="Images/Monica.jpg" style="border-radius: 2000px; height: 30px; width: 30px;">
             </div>
 
             <div style="padding: 10px; color:white">
             <a>1w</a>
             </div>
 
-            <div style="padding: 10px; color: white">
-              <a>What</a>
+            <div style="padding: 10px; color: white;">
+              <img src="Images/chatbubble.png" style="width: 20px; height:20px;">
               <a>1</a>
             </div>
 
